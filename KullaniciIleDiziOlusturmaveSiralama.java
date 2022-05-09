519f75069b9a0b8a2598d966997678e3f86631af
Servet Arslan <arslannservett@gmail.com>
2022-05-09 20:41:42 +0300

    Algorithm updated

--- KullaniciIleDiziOlusturmaveSiralama.java
+++ KullaniciIleDiziOlusturmaveSiralama.java
@@ -1,7 +1,9 @@
+package Projeler;
 /*
 Java dilinde, dizideki elemanları küçükten büyüğe sıralayan programı yazınız. Dizinin boyutunu ve
 dizinin elemanlarını kullanıcıdan alınız.
  */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,6 +18,22 @@ public class KullaniciIleDiziOlusturmaveSiralama {
         }
     }
 
+    static int[] sortArray(int[] array) {
+        int len = array.length;
+        int temp;
+
+        for (int i = 0; i < len; ++i) {
+            for (int j = (i + 1); j < len; ++j) {
+                if (array[j] < array[i]) {
+                    temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -27,11 +45,14 @@ public class KullaniciIleDiziOlusturmaveSiralama {
         list = new int[arrayLength];
 
         createAray(list);
-
+        System.out.println("Sıralama: " + Arrays.toString(sortArray(list)));
+/*
         Arrays.sort(list);
         System.out.print("Sıralama: ");
         for (int val : list) {
             System.out.print(val + " ");
         }
+
+ */
     }
 }
