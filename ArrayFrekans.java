0e5a980d7edb7425e9509f354a5faeec1c1fe2d5
Servet Arslan <arslannservett@gmail.com>
2022-05-09 21:29:54 +0300

    Updated algorithm

--- ArrayFrekans.java
+++ ArrayFrekans.java
@@ -5,21 +5,11 @@ import java.util.Arrays;
 
 public class ArrayFrekans {
 
-    static boolean isThere(int[] arr, int value) {
-        for (int val : arr) {
-            if (val == value) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
         int[] list = {10, 20, 20, 10, 10, 20, 5, 20};
         Arrays.sort(list);
 
         int count;
-
 
         for (int i=0 ; i < list.length; i++) {
             count = 0;
@@ -33,7 +23,6 @@ public class ArrayFrekans {
             i += (count-1);
             System.out.println(list[i] + " sayısı dizide " + count + " defa tekrar ediyor.");
         }
-
 
     }
 }
